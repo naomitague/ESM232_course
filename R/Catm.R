382f237fce6646ee65cc594428704eebae9269bd
Naomi Tague <ctague@bren.ucsb.edu>
2022-04-26 08:02:00 -0700

    added sobol

--- R/Catm.R
+++ R/Catm.R
@@ -4,22 +4,24 @@
 #' @param       v windspeed (m/s)
 #' @param      height vegetation height (m)
 #' @param       zm measurement height of wind (m) (default 30)
-#' @param      mult_zo scalar for roughness (default 0.1)
-#' @param      mult_zd scalar for zero plane displacement (default 0.7)
+#' @param      k_o scalar for roughness (default 0.1)
+#' @param      k_d scalar for zero plane displacement (default 0.7)
 #' @author Naomi
-#' 
+#'
 #' @return  Conductance (mm/s)
 
-Catm = function(v, height, zm=30, mult_zo=0.1, mult_zd=0.7) {
-    
-  
-    zd = mult_zd*height
-    zo = mult_zo*height
-    
+Catm = function(v, height, zm_add=200, k_o=0.1, k_d=0.7) {
+
+
+    zd = k_d*height
+    zo = k_o*height
+
+    zm = height+zm_add
+
     zd = ifelse(zd < 0, 0, zd)
     Ca = ifelse(zo > 0,
      v / (6.25*log((zm-zd)/zo)**2),0)
-  
+
 # convert to mm
     Ca = Ca*1000
    return(Ca)
