58b121b3d26ee63b582aa38f5cbd43f77948dae5
Naomi Tague <ctague@bren.ucsb.edu>
2024-05-02 12:19:37 -0700

    some recent updates for dynamic models

--- R/discrete_logistic_pop.R
+++ R/discrete_logistic_pop.R
@@ -5,14 +5,13 @@
 #' @param K carrying capacity
 #' @return population after T
 
-discrete_logistic_pop = function(P0,r,K, T=10) {
+discrete_logistic_pop = function(P0,r,T) {
 	# set initial population
   pop=P0
-  
+
   # recalculate population for each time step
 	for (i in 1:T) {
 	pop = pop + r*pop
-	pop = ifelse(pop > K, K, pop)
 	}
 	return(pop)
 }
