5174f39c30bc27fee9bb2b7328b15bc528515820
naomitague <ctague@bren.ucsb.edu>
2025-04-28 17:01:06 -0700

    updates to simple dynamic

--- R/exppopK.R
+++ R/exppopK.R
@@ -2,10 +2,15 @@
 #' @param T  period of growth
 #' @param P initial population
 #' @param r intrinsic growth rate
+#' @param K maximum pop (carrying capacity)
 #' @return population at time T
 #'
-exppop <- function(T, P0, r) {
+exppopK <- function(T, P0, r, K) {
   # analytical calculation of population
   P <- P0 * exp(r * T)
-   return(P)
+  # check to see if greater than carrying capacity
+  if (P > K) {
+    P <- K
+  }
+  return(P)
 }
