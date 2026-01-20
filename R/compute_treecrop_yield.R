9feb1450818f8b075e21976b23eac206ebcbb0e1
naomitague <tague@ucsb.edu>
2026-01-20 15:21:14 -0800

    added solution for all trees from assignment with Lobell paper

--- R/compute_treecrop_yield.R
+++ R/compute_treecrop_yield.R
@@ -14,45 +14,119 @@
 #'
 # UNDER DEVELOPMENT
 
-compute_treecrop_yield <- function(clim, Tmin_month_coeff, Tmax_month_coeff, Precip_month_coeff, Precip_month_coeff2, Intercept, crop = "Almond") {
-  # extracted required climate variables
+compute_treecrop_yield <- function(clim, crop = 2, override_defaults = TRUE,
+                                   Tmin_month_coeff = NULL, Tmin_month_coeff2 = NULL,
+                                   Tmax_month_coeff = NULL, Tmax_month_coeff2 = NULL,
+                                   P_month_coeff = NULL, P_month_coeff2 = NULL,
+                                   Intercept = NULL) {
+  # initialize indices for different trees
+  winegrapes=1
+  almonds=2
+  tablegrapes=3
+  oranges=4
+  walnuts=5
+  avocados=6
 
-  crop_index <- case_when(crop) {
-    crop == "Almond" ~ 1.0
-    crop == "Walnut" ~ 0.8
-    crop == "Pistachio" ~ 0.6
-    TRUE ~ 1.0
+  if (is.null(Tmin_month_coeff)) {
+    Tmin_month_coeff<- matrix(0, nrow = 6, ncol = 12)
+    # set default non-zero values
+    Tmin_month_coeff[winegrapes, 4] <- 2.65
+    Tmin_month_coeff[almonds, 2] <- -0.015
+    Tmin_month_coeff[tablegrapes, 7] <- 6.93
+    Tmin_month_coeff[oranges, 12] <- 1.08
+    Tmin_month_coeff[walnuts, 11] <- 0.68
+    Tmin_month_coeff[avocados, 8] <- 17.71
   }
 
+  if (is.null(Tmin_month_coeff2)) {
+    Tmin_month_coeff2<- matrix(0, nrow = 6, ncol = 12)
+    # set default non-zero values
+    Tmin_month_coeff2[winegrapes, 4] <- -0.17
+    Tmin_month_coeff2[almonds, 2] <- -0.0046
+    Tmin_month_coeff2[tablegrapes, 7] <- -0.19
+    Tmin_month_coeff2[oranges, 12] <- --0.2
+    Tmin_month_coeff2[walnuts, 11] <- -0.020
+    Tmin_month_coeff2[avocados, 8] <- -0.29
+  }
+
+  if (is.null(Tmax_month_coeff)) {
+    Tmax_month_coeff <- matrix(0, nrow = 6, ncol = 12)
+      Tmax_month_coeff[walnuts, 11] <- 0.68
+      Tmax_month_coeff[avocados, 8] <- 17.71
+  }
+
+  if (is.null(Tmax_month_coeff2)) {
+    Tmax_month_coeff2 <- matrix(0, nrow = 6, ncol = 12)
+       Tmax_month_coeff2[walnuts, 11] <- -0.020
+      Tmax_month_coeff2[avocados, 8] <- -0.29
+  }
+
+  if (is.null(P_month_coeff)) {
+    P_month_coeff <- matrix(0, nrow = 6, ncol = 12)
+       P_month_coeff[winegrapes, 6] <- 4.78
+    P_month_coeff[winegrapes, 9] <- -2.24
+        P_month_coeff[almonds, 1] <- -0.07
+        P_month_coeff[tablegrapes, 1]  <- 0.035
+    P_month_coeff[tablegrapes, 10] <- 1.71
+        P_month_coeff[oranges, 5] <- 4.99
+      P_month_coeff[walnuts, 2] <- 0.038
+    P_month_coeff[avocados, 10] <- 1.00
+  }
+
+  if (is.null(P_month_coeff2)) {
+    P_month_coeff2 <- matrix(0, nrow = 6, ncol = 12)
+
+    P_month_coeff2[winegrapes, 6] <- -4.93
+    P_month_coeff2[winegrapes, 9] <- 1.54
+    P_month_coeff2[almonds, 1] <- 0.0043
+    P_month_coeff2[tablegrapes, 1]  <- 0.024
+    P_month_coeff2[tablegrapes, 10] <- -0.673
+    P_month_coeff2[oranges, 5] <- -1.97
+    P_month_coeff2[walnuts, 2] <- -0.0051
+    P_month_coeff2[avocados, 10] <- -0.31
+  }
+
+  if (is.null(Intercept)) {
+    Intercept <- c(-10.5,0.28,-73.89,-2.47,-5.83,-288.09)
+  }
+
+# create a growing year to deal with negative months
+  clim <- clim %>% mutate(
+    gwy = ifelse(month >= 8, year + 1, year)
+  )
+
   clim_month_allyears <- clim %>%
-    group_by(month, year) %>%
+    group_by(month, gwy) %>%
     dplyr::summarize(
       tmin_c = min(tmin_c), tmax_c = max(tmax_c),
       precip = sum(precip), .groups = "drop"
     )
 
   # compute yield
-  compute_yield <- function(Tmin_month_coeff, Tmax_month_coeff,
-                            Precip_month_coeff, Precip_month_coeff2, Intercept, year, clim_month_allyears) {
-    subset(clim_month_allyears, year == year)
+  compute_yield <- function(Tmin_month_coeff, Tmin_month_coeff2, Tmax_month_coeff, Tmax_month_coeff2,
+                            Precip_month_coeff, Precip_month_coeff2, Intercept, clim_month_allyears, tgwy) {
+    clim_month = subset(clim_month_allyears, gwy==tgwy)
+    if (nrow(clim_month) == 12) {
     yield <-
-      Tmin_month_coeff * clim_month$tminc +
-      Tmax_month_coeff * clim_month$tmaxc +
-      Tmin_month_coeff2 * clim_month$tminc**2 +
-      Tmax_month_coeff2 * clim_month$tmaxc**2 +
-      Precip_month_coeff * clim_month$Precip + intercep
-    Precip_month_coeff2 * clim_month$Precip**2 + intercep
-    Pcoeff1 * Jan_P + Pcoeff2 * Jan_P**2 + intercep
-    return(yield)
+      sum(Tmin_month_coeff * clim_month$tmin_c) +
+      sum(Tmax_month_coeff * clim_month$tmax_c) +
+      sum(Tmin_month_coeff2 *clim_month$tmin_c)**2 +
+      sum(Tmax_month_coeff2 * clim_month$tmax_c)**2 +
+      sum(Precip_month_coeff * clim_month$precip) +
+      sum(Precip_month_coeff2 * clim_month$precip**2) + Intercept
+    }
+    else
+      yield=NULL
+
+     return(yield)
   }
 
-  res <- unique(clim_month_allyears$year) %>%
-    map_dfc(~ compute_yield(
-      year = .x, Tmin_month_coeff, Tmax_month_coeff,
-      Precip_month_coeff, Precip_month_coeff2, Intercept, clim_month_allyears
+  res <- unique(clim_month_allyears$gwy) %>%
+    map(~ compute_yield(
+      Tmin_month_coeff[crop,], Tmin_month_coeff2[crop,],  Tmax_month_coeff[crop,], Tmax_month_coeff2[crop,],
+      P_month_coeff[crop,], P_month_coeff2[crop,], Intercept[crop], clim_month_allyears, tgwy=.x
     ))
 
-
-
-  return(list(maxyield = max(yield), minyield = min(yield), meanyield = mean(yield)))
+  res=unlist(res)
+  return(list(maxyield = max(res), minyield = min(res), meanyield = mean(res)))
 }
