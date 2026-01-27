7544016531133caa0b4ca0eded45ca6e6bf53750
naomitague <tague@ucsb.edu>
2026-01-26 17:41:36 -0800

    added new material for lecture 7 including scaffold for live coding

--- R/simple_erosion.R
+++ R/simple_erosion.R
@@ -3,16 +3,16 @@
 #' Computes amount of erosion based on rainfall intensity and soil properties
 #' @param I rainfall intensity (mm/hr)
 #' @param f infiltration capacity (mm/hr)
-#' @param a exponent scaling runoff effect on erosion, default = 2
-#' @param k exponent scaling infiltration effect on runoff default = 4
+#' @param k exponent scaling runoff effect on erosion, default = 4
 
 #' @author Naomi
 #' @return Erosion value (kg sediment / hr)
 
-simple_erosion <- function(I, f, k = 4, a = 2) {
+simple_erosion <- function(I, f, k = 4) {
 # first compute runoff
-  q <- pmax(0, I - f)^k
-#  then erosion given runoff (a > 1)
-  erosion = q^a
+  q <- pmax(0, I - f)
+# then erosion
+  erosion <- q^k
+
   return(erosion)
 }
